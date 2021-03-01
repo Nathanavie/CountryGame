6c347a044bf0b436bdac3819cc1857c29ebe759a
Nathan Atherton <nathanatherton2@gmail.com>
2021-03-01 20:29:32 +0000

    Add in lots of functionality to the game component

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import { getData } from '../utils';
+import React, { useState } from 'react';
+import { string, func, number } from 'prop-types';
+import Flag from './shared/Flag';
+import Paragraph from './shared/Paragraph';
+import GuessCountry from './GuessCountry';
+import CorrectAnswer from './CorrectAnswer';
 
-function Game() {
-  function getRandomCountry(list) {
-    // get length and choose a random number between it and 0
-    const randomNumber = Math.floor(Math.random() * list.length + 1);
-    console.log(randomNumber);
+function Game(props) {
+  const {
+    name,
+    newCountry,
+    flag,
+    lives,
+    removeLife,
+    endGame,
+    score,
+    setScore,
+  } = props;
+  const [guess, setGuess] = useState('');
+  const [correct, setCorrect] = useState(false);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setGuess(value);
   }
 
-  function getCountries() {
-    getData('https://restcountries.eu/rest/v2/').then((res) => {
-      getRandomCountry(res);
-    });
+  function getNextCountry() {
+    setGuess('');
+    newCountry();
+  }
+
+  function checkAnswer() {
+    if (!guess) {
+      return;
+    }
+    if (name.toLowerCase() === guess.toLowerCase()) {
+      setScore(score + 1);
+      setCorrect(true);
+      getNextCountry();
+    } else {
+      console.log(
+        `answer given was ${guess}, when it the correct answer is ${name}`
+      );
+      if (lives === 1) {
+        console.log('lost the last life!!');
+        removeLife();
+        endGame();
+      } else {
+        removeLife();
+      }
+    }
+  }
+  function nextCountry() {
+    setCorrect(false);
+    getNextCountry();
   }
   return (
     <div>
-      <button type="button" onClick={getCountries}>
-        click
-      </button>
+      <Paragraph wording={`Score: ${score}`} />
+      <Paragraph wording={`Lives remaining: ${lives}`} />
+      <Flag flag={flag} />
+      {!correct ? (
+        <GuessCountry
+          checkAnswer={checkAnswer}
+          handleChange={handleChange}
+          guess={guess}
+        />
+      ) : (
+        <CorrectAnswer nextCountry={nextCountry} answer={name} />
+      )}
     </div>
   );
 }
+
+Game.propTypes = {
+  name: string.isRequired,
+  newCountry: func.isRequired,
+  flag: string.isRequired,
+  lives: number.isRequired,
+  score: number.isRequired,
+  setScore: func.isRequired,
+  removeLife: func.isRequired,
+  endGame: func.isRequired,
+};
 
 export default Game;
