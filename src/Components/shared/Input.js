9834ab4403351133eb8a7f2dcf4c3926ad3e05b0
Nathan Atherton <nathanatherton2@gmail.com>
2021-03-08 18:06:22 +0000

    Update Input.js to allow a placeholder to be sent with a default one used if none sent.

--- src/Components/shared/Input.js
+++ src/Components/shared/Input.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { string, func } from 'prop-types';
 
 function Input(props) {
-  const { handleChange, value, type } = props;
-  return <input onChange={handleChange} type={type} value={value} />;
+  const { handleChange, value, type, placeholder } = props;
+  return (
+    <input
+      onChange={handleChange}
+      type={type}
+      value={value}
+      placeholder={placeholder}
+    />
+  );
 }
 
 Input.propTypes = {
   handleChange: func.isRequired,
   value: string.isRequired,
   type: string.isRequired,
+  placeholder: string,
+};
+
+Input.defaultProps = {
+  placeholder: 'Enter text here...',
 };
 
 export default Input;
