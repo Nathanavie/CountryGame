9d09a95278136d28991f309f1751a7cf101efb24
Nathan Atherton <nathanatherton2@gmail.com>
2021-03-08 18:04:45 +0000

    Update App.js
    * Add in new Button component
    * Put difficulties into an array to map though - this will render the buttons using the new component to set the difficulty
    * Update the way states change when dependent on the current state
    * create a resetGame function
    * pass altSpellings into Game component too

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Paragraph from './Components/shared/Paragraph';
 import Heading from './Components/shared/Heading';
+import Button from './Components/shared/Button';
 import Game from './Components/Game';
 import { getData } from './utils';
 
@@ -12,6 +13,12 @@ function App() {
   const [country, setCountry] = useState();
   const [lives, setLives] = useState(10);
   const [score, setScore] = useState(0);
+  const diffs = [
+    { id: '10lives', wording: '10 lives', lives: 10 },
+    { id: '5lives', wording: '5 lives', lives: 5 },
+    { id: '3lives', wording: '3 lives', lives: 3 },
+    { id: 'onelife', wording: 'One life', lives: 1 },
+  ];
 
   function getCountries() {
     getData('https://restcountries.eu/rest/v2/')
@@ -40,41 +47,39 @@ function App() {
     getCountries();
   }, [!countries]);
   function setDifficulty(amount) {
-    setLives(amount);
+    setLives(Number(amount));
   }
   function endGame() {
     setGame('ended');
   }
   function removeLife() {
-    setLives(lives - 1);
+    setLives((currLives) => currLives - 1);
+  }
+  function resetGame() {
+    setLives(0);
+    setScore(0);
+    setGame('pregame');
   }
   if (!isLoaded) {
     return <div>Loading...</div>;
   }
+
   if (game === 'pregame') {
     return (
       <div className="App">
         <Heading wording="Guess the country" />
         <div className="container">
-          <Paragraph wording="no countries whoops" />
           <div id="difficulties">
             <Paragraph wording="Select the amount of lives you want" />
-            <button type="button" onClick={() => setDifficulty(10)}>
-              10 lives
-            </button>
-            <button type="button" onClick={() => setDifficulty(5)}>
-              5 lives
-            </button>
-            <button type="button" onClick={() => setDifficulty(3)}>
-              3 lives
-            </button>
-            <button type="button" onClick={() => setDifficulty(1)}>
-              One life
-            </button>
+            {diffs.map((diff) => (
+              <Button
+                id={diff.id}
+                wording={diff.wording}
+                handleClick={() => setDifficulty(`${diff.lives}`)}
+              />
+            ))}
           </div>
-          <button type="button" onClick={startGame}>
-            Start Game
-          </button>
+          <Button wording="Start Game" handleClick={startGame} />
         </div>
       </div>
     );
@@ -85,6 +90,7 @@ function App() {
         <Heading wording="Guess the country" />
         <div className="container">
           <Paragraph wording={`Game over, your score was ${score}`} />
+          <Button wording="Reset game" handleClick={resetGame} />
         </div>
       </div>
     );
@@ -96,6 +102,7 @@ function App() {
         <div className="container">
           <Game
             name={country.name}
+            altSpellings={country.altSpellings}
             flag={country.flag}
             newCountry={getRandomCountry}
             lives={lives}
